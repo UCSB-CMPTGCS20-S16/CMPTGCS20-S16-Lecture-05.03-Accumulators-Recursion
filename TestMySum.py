bc86c9a390dff5ac438e6625f1f35ccda30f3f65
Phill Conrad <pconrad@cs.ucsb.edu>
2016-05-03 11:27:24 -0700

    Example of the __name__=='__main__' idiom

---
+++ TestMySum.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestMySum(unittest.TestCase):
+
+    def test_mySum_a(self):
+        self.assertEqual(mySum([]), 0)
+
+    def test_mySum_b(self):
+        self.assertEqual(mySum([7]), 7)
+
+    def test_mySum_c(self):
+        self.assertEqual(mySum([7, 3]), 10)
+
+    def test_mySum_c(self):
+        self.assertEqual(mySum([7, 3, 1, 2, 3]), 16)
+
+
+if __name__=="__main__":
+    print "Thanks for running me!"
+else:
+    print "Thanks for importing me!"
